b6af040a39bf6293d2c7ea09d99d7e4e2a80a0f7
=aminejelass <=aminejelassi95@gmail.com>
2022-01-18 16:29:38 +0100

    implementent rating

--- index.js
+++ index.js
@@ -74,9 +74,9 @@ const auth = require("./routers/auth-routes");
 const workers = require("./routers/workers");
 const companies = require("./routers/companies");
 const events = require("./routers/events");
-
+const CompanyEvents = require("./routers/companyRoutes/events")
 var company = require("./routers/company");
-// var worker = require("./routers/worker");
+var worker = require("./routers/worker");
 var nodemailer = require("./routers/nodemailer");
 
 const addEvent = require("./routers/addEvent");
@@ -84,8 +84,9 @@ const addEvent = require("./routers/addEvent");
 app.get("/", (req, res, next) => {
   res.send("hello from express");
 });
-
+app.use("/companyevetns",CompanyEvents)
 app.use("/auth", auth);
+app.use("/worker", worker);
 app.use("/workers", workers);
 app.use("/companies", companies);
 app.use("/events", events); // good !
@@ -94,6 +95,7 @@ app.use("/addEvent", addEvent); // ???
 
 app.use("/nodemailer", nodemailer); // ???
 app.use("/company", company); // ???
+
 
 /****************************** Middleware that Catch the "Wrong Endpoint" ******************************/
 // Catch 404 errors and forward them to error handler
